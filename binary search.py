7a5ea4850009789d2dc9a0fe1b83355f488d01ea
syam <borusulokesh0350@gmail.com>
2025-08-05 14:48:11 +0530

    some basic practice problems in python

---
+++ binary search.py
@@ -0,0 +1,15 @@
+def bs(arr,left,right,target):
+    while left<=right:
+         mid=(left+right)//2
+         if arr[mid]==target:
+             return mid
+         elif arr[mid]<target:
+             left=mid+1
+         else:
+             right=mid-1
+    return-1    
+arr=[2,3,4,5,6,7,]
+target=7
+left=0
+right=len(arr)-1
+print(bs(arr,left,right,target))
