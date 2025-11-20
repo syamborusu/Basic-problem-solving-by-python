cc106fe70a4fbd64e58ce146be1db6ee186ca32f
syam <borusulokesh0350@gmail.com>
2025-11-20 19:05:43 +0530

    sorting algorithams

--- patterns/retriangle.py
+++
@@ -1,5 +0,0 @@
-n=int(input("value="))
-for i in range(n):
-    for j in range(n-i):      
-        print("#",end=" ")
-    print()
--- patterns/triangle.py
+++
@@ -1,5 +0,0 @@
-n=int(input("value="))
-for i in range(n):         #outter loop,row
-    for j in range(i+1):    #inner loop,colum
-        print("#",end=" ")  #end for same line
-    print()
