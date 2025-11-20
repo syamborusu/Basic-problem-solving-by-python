d9338c24b48d9df40d7f84b7d7a4f922c283670a
syam <borusulokesh0350@gmail.com>
2025-11-20 19:00:33 +0530

    revised

--- binary search.py
+++ binary search.py
@@ -1,16 +1,18 @@
 #bianry search
 def bs(arr,left,right,target):
+    if target not in arr:
+        return -1
     while left<=right:
          mid=(left+right)//2
          if arr[mid]==target:
              return mid
          elif arr[mid]<target:
-             left=mid+1
+             left=mid
          else:
-             right=mid-1
+             right=mid
     return-1    
 arr=[10,20,30,40,50,60,70,80,90]
-target=40
+target=50
 left=0
 right=len(arr)-1
 print(bs(arr,left,right,target))
